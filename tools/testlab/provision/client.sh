9ea32e13162a7b99dade0196125836376091f752
cheretbe <a.a.orlov@gmail.com>
2019-01-30 14:17:40 +0200

    Add CentOS to the testlab

--- tools/testlab/provision/client.sh
+++ tools/testlab/provision/client.sh
@@ -6,13 +6,20 @@ if which apt-get &> /dev/null; then
   apt-get -y -q update
   apt-get -y -q upgrade
   apt-get -y -q install python3-pip
+  PIP3_PATH=$(which pip3)
 elif which yum &> /dev/null; then
   yum -y update
+  # yum -y install epel-release
+  # localinstall doesn't return error code if the package is already installed
+  yum localinstall -y https://centos7.iuscommunity.org/ius-release.rpm
+  yum install -y python36u python36u-pip
+  #yum -y install python-pip
+  PIP3_PATH=$(which pip3.6)
 else
   >&2 echo "Unsupported package manager"
   exit 1
 fi
 
-pip3 --version
+${PIP3_PATH} --version
 
 exit 0

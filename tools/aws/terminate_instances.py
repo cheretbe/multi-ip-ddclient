8d375fd09ae1a07fd6e368f326e3518c72e61175
cheretbe <a.a.orlov@gmail.com>
2019-03-20 13:52:53 +0200

    Convert instance deletion script to Python

---
+++ tools/aws/terminate_instances.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import boto3
+
+parser = argparse.ArgumentParser(description="AWS instance deletion helper")
+
+parser.add_argument("-n", "--nowait", dest="no_wait", action="store_true",
+    default=False, help="do not wait for action instance(s) termination")
+
+options = parser.parse_args()
+
+ec2_client = boto3.client("ec2")
+
+print("Querying instances")
+response = ec2_client.describe_instances(
+    Filters=[{"Name": "tag:Name", "Values": ["ddclient-test-client", "ddclient-test-router"]}]
+)
+instances = [i["Instances"][0] for i in response["Reservations"]]
+
+running_instance_ids = []
+for instance in instances:
+    if instance["State"]["Name"] == "running":
+        print("Terminating instance '{}'".format(instance["InstanceId"]))
+        ec2_client.terminate_instances(InstanceIds=[instance["InstanceId"]])
+        running_instance_ids += [instance["InstanceId"]]
+
+if (not options.no_wait) and running_instance_ids:
+    print("Waiting for instance(s) to terminate...")
+    waiter = ec2_client.get_waiter("instance_terminated")
+    waiter.wait(InstanceIds=running_instance_ids)
+
+print("Done")

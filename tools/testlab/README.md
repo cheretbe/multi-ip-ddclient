d34b8644dac85875819312b531fa4dafe09876a6
cheretbe <a.a.orlov@gmail.com>
2019-04-10 20:32:36 +0200

    Finish sending of env variables to Vagrant VM

---
+++ tools/testlab/README.md
@@ -0,0 +1,4 @@
+```shell
+# https://stackoverflow.com/questions/1198378/ssh-command-execution-doesnt-consider-bashrc-bash-login-ssh-rc/4350185#4350185
+vagrant ssh -- bash -lc export
+```
--- tools/testlab/provision/env_vars.sh
+++ tools/testlab/provision/env_vars.sh
@@ -2,12 +2,18 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
-grep -qxF 'source ~/custom_env_vars.sh' ~/.bashrc || printf "\nsource ~/custom_env_vars.sh\n" >> ~/.bashrc
+grep -qxF 'source ~/custom_env_vars.sh' ~/.profile || printf "\nsource ~/custom_env_vars.sh\n" >> ~/.profile
 
 echo "Setting custom environment variables"
-echo "# Custom env variables" > ~/custom_env_vars.sh
+echo "# Custom environment variables" > ~/custom_env_vars.sh
 for env_var in ${1}; do
   echo "  ${env_var}"
+  if [ -z "${!env_var}" ]; then
+    >&2 echo "Environment variable '${env_var}' is not defined"
+    >&2 echo "For this Vagrantfile to work correctly the following " \
+      "environment variables need to be defined: ${1}"
+    exit 1
+  fi
   echo "export ${env_var}=${!env_var}" >>~/custom_env_vars.sh
 done
 
